ada585e9f8b7675b6d54f068e40ec3330a97704e
dekan <dekansarwat33@gmail.com>
2023-03-27 14:40:31 +0300

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   const fetchBotResponse = async () => {
     const { data } = await axios.post(
-      "http://localhost:4000",
+      "https://chatgpt-ai-app-main.onrender.com",
       // "http://localhost:5173",
       { input },
       {
